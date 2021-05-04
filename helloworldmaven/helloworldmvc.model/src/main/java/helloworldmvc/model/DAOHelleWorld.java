a83104b7a383a8c70151cff486d8d6cfec54339e
Joulebeco <82848851+Joulebeco@users.noreply.github.com>
2021-05-04 10:52:23 +0200

    Signed-off-by: Joulebeco <82848851+Joulebeco@users.noreply.github.com>

--- helloworldmaven/helloworldmvc.model/src/main/java/helloworldmvc/model/DAOHelleWorld.java
+++ helloworldmaven/helloworldmvc.model/src/main/java/helloworldmvc/model/DAOHelleWorld.java
@@ -1,6 +1,10 @@
 package helloworldmvc.model;
 
 public class DAOHelleWorld {
+	private String FileName = "Helloworld.txt";
+	private DAOHelloWorld instance = null;
+	private String helloWorldMessage =null;
+	
 	
 
 }
